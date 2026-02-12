bf09163f1a2a23d04d8b719a12c4f6282ced9e19
yazalisanjesh4-spec <https://github.com/yazalisanjesh4-spec/yahin>
2026-02-12 14:11:50 +0530

    yahin setup

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home(){
+  return(
+    <main className="p-4">
+    <h1 className="text-2xl font-bold">
+      Yahin Local Clothes Delivered Fast
+    </h1>
+    </main>
+  )
+}
