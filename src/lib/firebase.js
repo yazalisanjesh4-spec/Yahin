60d9d599f852bb2342e9fe50bf22b79bd846b726
sanjesh yazali <yazalisanjesh4@gmail.com>
2026-02-13 18:23:39 +0530

    api key changed and protected

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey:  "AIzaSyDJl-SZelbrUbRkZXkOtvtnfNjGBDjt6MI",
-  authDomain: "yahin-ed6d9.firebaseapp.com",
-  projectId: "yahin-ed6d9",
-  storageBucket: "yahin-ed6d9.firebasestorage.app",
-  messagingSenderId: "278365702103",
-  appId: "1:278365702103:web:6fb38af773d1ea5203ed92",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
